58770b5f30edc177d6053a41e108c8fecdc1a326
noahdotpy <80379684+noahdotpy@users.noreply.github.com>
2022-08-26 11:11:51 +1000

    Delete barrier block (if any) underneath player when game starts

--- src/main/java/me/diligord/battleboxclone/events/GameStartEvent.java
+++ src/main/java/me/diligord/battleboxclone/events/GameStartEvent.java
@@ -2,6 +2,10 @@ package me.diligord.battleboxclone.events;
 
 import me.diligord.battleboxclone.BattleBoxClone;
 import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
@@ -35,6 +39,13 @@ public class GameStartEvent extends Event implements Cancellable {
         broadcast(Component.text("Go!"));
 
         plugin.gameStarted = true;
+
+        Location teleportLocation = (Location) plugin.getConfig().get("teams.blue.teleport-location");
+
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            Block blockUnder = teleportLocation.clone().add(new Location(teleportLocation.getWorld(), 0, -1, 0)).getBlock();
+            if (blockUnder.getType() == Material.BARRIER) blockUnder.setType(Material.AIR);
+        });
     }
 
 }
